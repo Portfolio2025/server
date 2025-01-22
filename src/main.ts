402f9d9e34cd7520153c232880ed1c3c073f3b40
Aleksei Kozlov <aleksei22891@gmail.com>
2025-01-22 02:49:18 +0200

    return old variant

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+
   const configService = app.get(ConfigService);
   app.setGlobalPrefix(configService.get<string>("API_NAME"))
+
+  app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors({
     origin: ['http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
