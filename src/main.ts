3231c341b59301b8069a51e303f4c4a88fd1e3f7
Aleksei Kozlov <aleksei22891@gmail.com>
2025-01-22 02:52:14 +0200

    added cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
 
@@ -10,12 +12,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.use(cookieParser());
+
   app.enableCors({
     origin: ['http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
+
   await app.listen(3000);
 }
 bootstrap();
